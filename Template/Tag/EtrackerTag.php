0a83025969fa48273c8e25f47324546f9d9e6777
Jacob Ransom <jacob@innocraft.com>
2024-12-13 11:13:47 +1300

    Updating tests after translation changes

--- Template/Tag/EtrackerTag.php
+++ Template/Tag/EtrackerTag.php
@@ -18,7 +18,7 @@ use Piwik\Validators\NumberRange;
 class EtrackerTag extends BaseTag
 {
     public const PARAM_ETRACKER_CONFIG = 'etrackerConfig';
-    
+
     public function getIcon()
     {
         return 'plugins/TagManager/images/icons/etracker.svg';
